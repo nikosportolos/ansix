c58f90c383724de80b5f2851cb153103f45d3f25
Nikos Portolos <nikos.portolos@gmail.com>
2023-05-03 00:35:09 +0300

    add missing text styles

--- lib/src/theme/text_style.dart
+++ lib/src/theme/text_style.dart
@@ -3,22 +3,31 @@ import 'package:ansix/src/theme/style.dart';
 class AnsiTextStyle {
   const AnsiTextStyle({
     this.bold = false,
+    this.boldUnderline = false,
+    this.dim = false,
+    this.inverse = false,
     this.italic = false,
-    this.underline = false,
     this.strikethrough = false,
+    this.underline = false,
   });
 
   final bool bold;
+  final bool boldUnderline;
+  final bool dim;
+  final bool inverse;
   final bool italic;
-  final bool underline;
   final bool strikethrough;
+  final bool underline;
 
   List<AnsiStyle> get styles {
     return <AnsiStyle>[
       if (bold) AnsiStyle.bold,
+      if (boldUnderline) AnsiStyle.boldUnderline,
+      if (dim) AnsiStyle.dim,
+      if (inverse) AnsiStyle.dim,
       if (italic) AnsiStyle.italic,
-      if (underline) AnsiStyle.underline,
       if (strikethrough) AnsiStyle.strikethrough,
+      if (underline) AnsiStyle.underline,
     ];
   }
 }

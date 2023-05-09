151e0ae02ce17a3142363cc4b2dba29f40558ab2
Nikos Portolos <nikos.portolos@gmail.com>
2023-05-09 04:18:08 +0300

    refactor AnsiTable - move list building to AnsiColumn

---
+++ lib/src/widgets/table/column.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+
+import 'package:ansix/src/core/core.dart';
+import 'package:ansix/src/theme/alignment.dart';
+import 'package:ansix/src/widgets/table/cell.dart';
+import 'package:ansix/src/widgets/table/row.dart';
+import 'package:ansix/src/widgets/text/text.dart';
+import 'package:collection/collection.dart';
+
+class AnsiTableColumn {
+  AnsiTableColumn({
+    required this.data,
+    this.fixedWidth,
+    this.defaultAlignment = AnsiTextAlignment.left,
+  }) {
+    final List<String> list = <String>[];
+    final List<AnsiTextAlignment> alignments = <AnsiTextAlignment>[];
+    int maxColumnWidth = fixedWidth ?? 0;
+
+    for (final Object? object in data) {
+      if (object is AnsiText) {
+        maxColumnWidth = max(object.width, maxColumnWidth);
+        list.addAll(object.formattedText.split(newLine));
+
+        alignments.addAll(<AnsiTextAlignment>[
+          for (int i = 0; i < object.padding.top; i++) defaultAlignment,
+          object.textAlignment,
+          for (int i = 0; i < object.padding.bottom; i++) defaultAlignment,
+        ]);
+
+        // TODO: Wrap line if AnsiText.fixedWidth is set
+      } else {
+        final String text = object.toString();
+        maxColumnWidth = max(text.length, maxColumnWidth);
+        final List<String> temp = text.split(newLine);
+        list.addAll(temp);
+
+        alignments.addAll(<AnsiTextAlignment>[
+          for (final String _ in temp) defaultAlignment,
+        ]);
+      }
+    }
+
+    rows = list.mapIndexed((int index, String text) {
+      return AnsiTableRow(
+        data: <AnsiText>[
+          AnsiTableCell(
+            text,
+            width: maxColumnWidth,
+            textAlignment: alignments[index],
+          ),
+        ],
+      );
+    }).toList(growable: false);
+  }
+
+  final List<Object?> data;
+  final int? fixedWidth;
+  final AnsiTextAlignment defaultAlignment;
+
+  late final List<AnsiTableRow> rows;
+}
--- lib/src/widgets/table/table.dart
+++ lib/src/widgets/table/table.dart
@@ -1,9 +1,6 @@
-import 'dart:math';
-
 import 'package:ansix/ansix.dart';
-import 'package:ansix/src/widgets/table/cell.dart';
+import 'package:ansix/src/widgets/table/column.dart';
 import 'package:ansix/src/widgets/table/row.dart';
-import 'package:collection/collection.dart';
 
 class AnsiTable {
   AnsiTable({
@@ -26,52 +23,38 @@ class AnsiTable {
   final List<AnsiTableRow> data;
   final String formattedText;
 
+  // factory AnsiTable.fromMap(
+  //   final Map<String, List<Object?>> data, {
+  //   final int? fixedWidth,
+  //   final AnsiBorder border = const AnsiBorder(),
+  //   final AnsiTextAlignment defaultAlignment = AnsiTextAlignment.left,
+  // }) {
+  //   for (final MapEntry<String, List<Object?>> column in data.entries) {
+  //     AnsiTableColumn(
+  //       data: <Object?>[column.key, ...column.value],
+  //       fixedWidth: fixedWidth,
+  //       defaultAlignment: defaultAlignment,
+  //     );
+  //   }
+  //
+  //   return AnsiTable(
+  //     border: border,
+  //   );
+  // }
+
   factory AnsiTable.fromList(
     final List<Object?> data, {
+    final int? fixedWidth,
     final AnsiBorder border = const AnsiBorder(),
     final AnsiTextAlignment defaultAlignment = AnsiTextAlignment.left,
   }) {
-    final List<String> list = <String>[];
-    final List<AnsiTextAlignment> alignments = <AnsiTextAlignment>[];
-    int maxColumnWidth = 0;
-
-    for (final Object? object in data) {
-      if (object is AnsiText) {
-        maxColumnWidth = max(object.width, maxColumnWidth);
-        list.addAll(object.formattedText.split(newLine));
-
-        alignments.addAll(<AnsiTextAlignment>[
-          for (int i = 0; i < object.padding.top; i++) defaultAlignment,
-          object.textAlignment,
-          for (int i = 0; i < object.padding.bottom; i++) defaultAlignment,
-        ]);
-
-        // TODO: Wrap line if AnsiText.fixedWidth is set
-      } else {
-        final String text = object.toString();
-        maxColumnWidth = max(text.length, maxColumnWidth);
-        final List<String> temp = text.split(newLine);
-        list.addAll(temp);
-
-        alignments.addAll(<AnsiTextAlignment>[
-          for (final String _ in temp) defaultAlignment,
-        ]);
-      }
-    }
-
     return AnsiTable(
       border: border,
-      data: list.mapIndexed((int index, String text) {
-        return AnsiTableRow(
-          data: <AnsiText>[
-            AnsiTableCell(
-              text,
-              width: maxColumnWidth,
-              textAlignment: alignments[index],
-            ),
-          ],
-        );
-      }).toList(growable: false),
+      data: AnsiTableColumn(
+        data: data,
+        fixedWidth: fixedWidth,
+        defaultAlignment: defaultAlignment,
+      ).rows,
     );
   }
 

2d117aea592319576f169f6d366f1c2a8ee91c93
Nikos Portolos <nikos.portolos@gmail.com>
2023-05-09 04:16:29 +0300

    create tests for AnsiTable

---
+++ test/widgets/table/mocks.dart
@@ -0,0 +1,145 @@
+part of 'table_test.dart';
+
+final Map<AnsiBorderType, String> tableMocks = <AnsiBorderType, String>{
+  AnsiBorderType.all: allBorderTableMock,
+  AnsiBorderType.header: headerBorderTableMock,
+  AnsiBorderType.inside: insideBorderTableMock,
+  AnsiBorderType.insideHorizontal: insideHorizontalBorderTableMock,
+  AnsiBorderType.insideVertical: insideVerticalBorderTableMock,
+  AnsiBorderType.none: noBorderTableMock,
+  AnsiBorderType.outside: outsideBorderTableMock,
+};
+
+final Map<AnsiBorderType, String> listMocks = <AnsiBorderType, String>{
+  AnsiBorderType.all: allBorderListMock,
+  AnsiBorderType.header: headerBorderListMock,
+  AnsiBorderType.inside: insideBorderListMock,
+  AnsiBorderType.insideHorizontal: insideHorizontalBorderListMock,
+  AnsiBorderType.insideVertical: insideVerticalBorderListMock,
+  AnsiBorderType.none: noBorderListMock,
+  AnsiBorderType.outside: outsideBorderListMock,
+};
+
+const String allBorderTableMock = '''
+┌───────────────┬───────────────┬───────────────┐
+│Name           │Hex            │RGB            │
+├───────────────┼───────────────┼───────────────┤
+│Red            │#ff0000        │(255, 0, 0)    │
+├───────────────┼───────────────┼───────────────┤
+│Green          │#008000        │(0, 128, 0)    │
+├───────────────┼───────────────┼───────────────┤
+│Blue           │#0000ff        │(0, 0, 255)    │
+└───────────────┴───────────────┴───────────────┘''';
+
+const String headerBorderTableMock = '''
+┌───────────────┬───────────────┬───────────────┐
+│Name           │Hex            │RGB            │
+└───────────────┴───────────────┴───────────────┘
+ Red             #ff0000         (255, 0, 0)     
+ Green           #008000         (0, 128, 0)     
+ Blue            #0000ff         (0, 0, 255)     
+''';
+
+const String insideBorderTableMock = '''
+Name           │Hex            │RGB            
+───────────────┼───────────────┼───────────────
+Red            │#ff0000        │(255, 0, 0)    
+───────────────┼───────────────┼───────────────
+Green          │#008000        │(0, 128, 0)    
+───────────────┼───────────────┼───────────────
+Blue           │#0000ff        │(0, 0, 255)    
+''';
+
+const String insideHorizontalBorderTableMock = '''
+Name            Hex             RGB            
+───────────────────────────────────────────────
+Red             #ff0000         (255, 0, 0)    
+───────────────────────────────────────────────
+Green           #008000         (0, 128, 0)    
+───────────────────────────────────────────────
+Blue            #0000ff         (0, 0, 255)    
+''';
+
+const String insideVerticalBorderTableMock = '''
+Name           │Hex            │RGB            
+Red            │#ff0000        │(255, 0, 0)    
+Green          │#008000        │(0, 128, 0)    
+Blue           │#0000ff        │(0, 0, 255)    
+''';
+
+const String noBorderTableMock = '''
+Name           Hex            RGB            
+Red            #ff0000        (255, 0, 0)    
+Green          #008000        (0, 128, 0)    
+Blue           #0000ff        (0, 0, 255)    
+''';
+
+const String outsideBorderTableMock = '''
+┌─────────────────────────────────────────────┐
+│Name           Hex            RGB            │
+│Red            #ff0000        (255, 0, 0)    │
+│Green          #008000        (0, 128, 0)    │
+│Blue           #0000ff        (0, 0, 255)    │
+└─────────────────────────────────────────────┘''';
+
+const String allBorderListMock = '''
+┌─────┐
+│Name │
+├─────┤
+│Red  │
+├─────┤
+│Green│
+├─────┤
+│Blue │
+└─────┘''';
+
+const String headerBorderListMock = '''
+┌─────┐
+│Name │
+└─────┘
+ Red   
+ Green 
+ Blue  
+''';
+
+const String insideBorderListMock = '''
+Name 
+─────
+Red  
+─────
+Green
+─────
+Blue 
+''';
+
+const String insideHorizontalBorderListMock = '''
+Name 
+─────
+Red  
+─────
+Green
+─────
+Blue 
+''';
+
+const String insideVerticalBorderListMock = '''
+Name 
+Red  
+Green
+Blue 
+''';
+
+const String noBorderListMock = '''
+Name 
+Red  
+Green
+Blue 
+''';
+
+const String outsideBorderListMock = '''
+┌─────┐
+│Name │
+│Red  │
+│Green│
+│Blue │
+└─────┘''';

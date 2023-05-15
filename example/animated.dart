c703b6d478e1a79aca45b0145ac77bcbd8c5de4c
Nikos Portolos <nikos.portolos@gmail.com>
2023-05-15 04:47:04 +0300

    create typewriter - for demo only

---
+++ example/animated.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+
+import 'package:ansix/ansix.dart';
+import 'package:ansix/src/widgets/typewriter/typewriter.dart';
+
+void main() {
+  const Duration duration = Duration(milliseconds: 200);
+  final Typewriter typewriter = Typewriter(duration: duration);
+
+  final String text = 'Hello ${'AnsiX'.bold()}';
+
+  typewriter.print(text);
+  sleep(duration);
+  typewriter.delete(
+    text.length,
+    deleteDuration: const Duration(milliseconds: 100),
+  );
+
+  final String colored = (StringBuffer()
+        ..writeWithForegroundColor('This ', AnsiColor.deepSkyBlue4)
+        ..writeWithForegroundColor('is ', AnsiColor.aquamarine1)
+        ..writeWithForegroundColor('a ', AnsiColor.green4)
+        ..writeWithForegroundColor('multi', AnsiColor.blue3)
+        ..writeWithForegroundColor('-', AnsiColor.darkOliveGreen1)
+        ..writeWithForegroundColor('color ', AnsiColor.greenYellow)
+        ..writeWithForegroundColor('text', AnsiColor.deepSkyBlue5)
+        ..writeWithForegroundColor('.', AnsiColor.white))
+      .toString();
+
+  typewriter.print(
+    colored,
+    printDuration: const Duration(milliseconds: 25),
+  );
+  sleep(const Duration(milliseconds: 200));
+  typewriter.delete(
+    colored.length,
+    deleteDuration: const Duration(milliseconds: 30),
+  );
+
+  final String styled = (StringBuffer()
+        ..writeStyled('This ', textStyle: const AnsiTextStyle(inverse: true))
+        ..writeStyled('is ', textStyle: const AnsiTextStyle(italic: true))
+        ..writeStyled('a ', textStyle: const AnsiTextStyle(bold: true))
+        ..writeStyled('text ', textStyle: const AnsiTextStyle(strikethrough: true))
+        ..writeStyled('with ', textStyle: const AnsiTextStyle(underline: true))
+        ..writeStyled('ANSI ', textStyle: const AnsiTextStyle(dim: true))
+        ..writeStyled('styles', textStyle: const AnsiTextStyle(bold: true)))
+      .toString();
+
+  typewriter.print(
+    styled,
+    printDuration: const Duration(milliseconds: 20),
+  );
+  sleep(const Duration(milliseconds: 100));
+  typewriter.delete(
+    styled.length,
+    deleteDuration: const Duration(milliseconds: 50),
+  );
+}
---
+++ lib/src/widgets/typewriter/typewriter.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+import 'package:ansix/ansix.dart';
+
+/// **Typewriter**
+class Typewriter {
+  Typewriter({
+    required this.duration,
+  });
+
+  final Duration duration;
+
+  void print(
+    final String text, {
+    final Duration? printDuration,
+  }) {
+    for (int i = 0; i < text.length; i++) {
+      stdout.write(text[i]);
+      sleep(printDuration ?? duration);
+    }
+  }
+
+  void delete(
+    final int characters, {
+    final Duration? deleteDuration,
+  }) {
+    for (int i = 0; i < characters; i++) {
+      stdout.write(AnsiEscapeCodes.backspace);
+      sleep(deleteDuration ?? duration);
+    }
+  }
+}

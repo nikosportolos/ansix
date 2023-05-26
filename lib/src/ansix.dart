a9ae8df819115617b2ecff256d2068ae00e1d984
Nikos Portolos <nikos.portolos@gmail.com>
2023-05-26 03:16:56 +0300

    #53 Add printJson method

--- lib/src/ansix.dart
+++ lib/src/ansix.dart
@@ -1,10 +1,16 @@
 // coverage:ignore-file
 
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:ansix/ansix.dart';
 import 'package:ansix/src/controller.dart';
 import 'package:ansix/src/formatter/formatter.dart';
 
 abstract class AnsiX {
   static final AnsiXController _controller = AnsiXController();
+  static const JsonEncoder encoder = JsonEncoder.withIndent('  ');
 
   /// Returns true if ANSI formatting is supported and enabled.
   static bool get isEnabled => _controller.isEnabled;
@@ -35,6 +41,46 @@ abstract class AnsiX {
     _controller.ensureSupportsAnsi(
       silent: silent,
       force: force,
+    );
+  }
+
+  static bool allowPrint = isDebugMode;
+
+  /// Prints a string representation of the object to the console
+  /// with the given styles and ANSI colors.
+  static printStyled(
+    final Object? object, {
+    required final AnsiTextStyle textStyle,
+    final AnsiColor foreground = AnsiColor.none,
+    final AnsiColor background = AnsiColor.none,
+  }) {
+    if (!allowPrint) {
+      return;
+    }
+    print('$object'.styled(
+      textStyle,
+      foreground,
+      background,
+    ));
+  }
+
+  /// Prints an indented string representation of the JSON
+  /// to the console with the given styles and ANSI colors.
+  static printJson(
+    final Object? object, {
+    final AnsiTextStyle textStyle = const AnsiTextStyle(),
+    final AnsiColor foreground = AnsiColor.none,
+    final AnsiColor background = AnsiColor.none,
+  }) {
+    if (!allowPrint) {
+      return;
+    }
+    print(
+      encoder.convert(object).styled(
+            textStyle,
+            foreground,
+            background,
+          ),
     );
   }
 }
--- lib/src/core/core.dart
+++ lib/src/core/core.dart
@@ -3,7 +3,6 @@
 export 'escape_codes.dart';
 export 'exceptions/exceptions.dart';
 export 'extensions/extensions.dart';
-export 'print.dart';
 
 bool get isDebugMode {
   bool value = false;

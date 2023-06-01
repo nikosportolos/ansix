e9cc1224ee8da223260b7ac740137571b218e67c
Nikos Portolos <nikos.portolos@gmail.com>
2023-06-02 00:49:54 +0300

    #87 Detect terminal size

--- lib/src/system/terminal.dart
+++ lib/src/system/terminal.dart
@@ -1,7 +1,29 @@
 import 'dart:io';
 
+/// **TerminalType**
 enum TerminalType { unknown, bash, cmd, powershell }
 
+/// **TerminalSize**
+///
+/// Provides information about the size of the attached terminal.
+class TerminalSize {
+  const TerminalSize({
+    required this.columns,
+    required this.lines,
+  });
+
+  final int columns;
+  final int lines;
+
+  static const TerminalSize $default = TerminalSize(columns: 80, lines: 24);
+
+  @override
+  String toString() => '($columns, $lines)';
+}
+
+/// **AnsiTerminal**
+///
+/// A library that provides information on the attached terminal.
 class AnsiTerminal {
   AnsiTerminal({
     final Stdout? out,
@@ -19,5 +41,22 @@ class AnsiTerminal {
   bool get attachedToValidStream {
     return stdioType(_stdout) == StdioType.terminal || //
         stdioType(_stdout) == StdioType.pipe;
+  }
+
+  /// Returns the [TerminalSize] of the attached terminal.
+  ///
+  /// If no terminal is attached,
+  /// the default terminal size (80, 24) will be returned instead.
+  TerminalSize get size {
+    try {
+      if (_stdout.hasTerminal) {
+        return TerminalSize(
+          columns: _stdout.terminalColumns,
+          lines: _stdout.terminalLines,
+        );
+      }
+    } catch (_) {}
+
+    return TerminalSize.$default;
   }
 }
--- test/system/terminal_test.dart
+++ test/system/terminal_test.dart
@@ -7,6 +7,15 @@ import 'package:test/test.dart';
 class MockStdOut extends Mock implements Stdout {
   @override
   bool get supportsAnsiEscapes;
+
+  @override
+  bool get hasTerminal;
+
+  @override
+  int get terminalColumns;
+
+  @override
+  int get terminalLines;
 }
 
 void main() {
@@ -14,30 +23,50 @@ void main() {
 
   setUp(() => resetMocktailState());
 
-  IOOverrides.runZoned(
-    () {
-      final AnsiTerminal terminal = AnsiTerminal(out: stdOut);
+  final AnsiTerminal terminal = AnsiTerminal(out: stdOut);
 
-      group('AnsiTerminal', () {
-        test('runsOnWindows', () {
-          expect(terminal.runsOnWindows, Platform.isWindows);
-        });
+  group('AnsiTerminal', () {
+    test('runsOnWindows', () {
+      expect(terminal.runsOnWindows, Platform.isWindows);
+    });
 
-        test('supportsAnsiEscapes=false', () {
-          when(() => stdOut.supportsAnsiEscapes).thenReturn(false);
-          expect(terminal.supportsAnsi, false);
-        });
-
-        test('supportsAnsiEscapes=true', () {
-          when(() => stdOut.supportsAnsiEscapes).thenReturn(true);
-          expect(terminal.supportsAnsi, true);
-        });
-
-        test('attachedToValidStream', () {
-          expect(terminal.attachedToValidStream, false); // TODO
-        });
+    group('supportsAnsiEscapes', () {
+      test('false', () {
+        when(() => stdOut.supportsAnsiEscapes).thenReturn(false);
+        expect(terminal.supportsAnsi, false);
       });
-    },
-    stdout: () => stdOut,
-  );
+
+      test('true', () {
+        when(() => stdOut.supportsAnsiEscapes).thenReturn(true);
+        expect(terminal.supportsAnsi, true);
+      });
+    });
+
+    test('attachedToValidStream', () {
+      expect(terminal.attachedToValidStream, false); // TODO
+    });
+
+    group('TerminalSize', () {
+      test('hasTerminal=false (returns default)', () {
+        when(() => stdOut.hasTerminal).thenReturn(false);
+        expect(terminal.size, TerminalSize.$default);
+        expect(
+          terminal.size.toString(),
+          '(${TerminalSize.$default.columns}, ${TerminalSize.$default.lines})',
+        );
+      });
+      test('hasTerminal=true (returns default)', () {
+        when(() => stdOut.hasTerminal).thenReturn(true);
+        expect(terminal.size, TerminalSize.$default);
+      });
+      test('hasTerminal=true', () {
+        const TerminalSize size = TerminalSize(columns: 120, lines: 100);
+        when(() => stdOut.hasTerminal).thenReturn(true);
+        when(() => stdOut.terminalColumns).thenReturn(size.columns);
+        when(() => stdOut.terminalLines).thenReturn(size.lines);
+        expect(terminal.size.columns, size.columns);
+        expect(terminal.size.lines, size.lines);
+      });
+    });
+  });
 }

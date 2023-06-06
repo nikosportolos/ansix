97337e905b06fd6b738cb6e4a4ae0f2e36b41630
Nikos Portolos <nikos.portolos@gmail.com>
2023-06-06 22:52:21 +0300

    #67 Refactor extensions

---
+++ lib/src/core/extensions/ansi_string_extensions.dart
@@ -0,0 +1,105 @@
+import 'package:ansix/ansix.dart';
+
+extension AnsiXShortcuts on String {
+  /// Adds [AnsiStyle.bold] text style on a string.
+  String bold() => withStyle(AnsiStyle.bold);
+
+  /// Adds [AnsiStyle.dim] text style on a string.
+  String dim() => withStyle(AnsiStyle.dim);
+
+  /// Adds [AnsiStyle.italic] text style on a string.
+  String italic() => withStyle(AnsiStyle.italic);
+
+  /// Adds [AnsiStyle.underline] text style on a string.
+  String underline() => withStyle(AnsiStyle.underline);
+
+  /// Adds [AnsiStyle.strikethrough] text style on a string.
+  String strikethrough() => withStyle(AnsiStyle.strikethrough);
+
+  /// Adds [AnsiColor.black] foreground color on a string.
+  String black() => colored(foreground: AnsiColor.black);
+
+  /// Adds [AnsiColor.red] foreground color on a string.
+  String red() => colored(foreground: AnsiColor.red);
+
+  /// Adds [AnsiColor.green] foreground color on a string.
+  String green() => colored(foreground: AnsiColor.green);
+
+  /// Adds [AnsiColor.yellow] foreground color on a string.
+  String yellow() => colored(foreground: AnsiColor.yellow);
+
+  /// Adds [AnsiColor.blue] foreground color on a string.
+  String blue() => colored(foreground: AnsiColor.blue);
+
+  /// Adds [AnsiColor.fuchsia] foreground color on a string.
+  String fuchsia() => colored(foreground: AnsiColor.fuchsia);
+
+  /// Adds [AnsiColor.aqua] foreground color on a string.
+  String aqua() => colored(foreground: AnsiColor.aqua);
+
+  /// Adds [AnsiColor.white] foreground color on a string.
+  String white() => colored(foreground: AnsiColor.white);
+}
+
+extension AnsiString on String {
+  /// Returns the string with no ANSI styles and colors.
+  String get unformatted => replaceAll(AnsiText.ansiMatcher, '');
+
+  /// Returns the length of the string with no ANSI styles and colors.
+  int get unformattedLength => unformatted.length;
+
+  /// Adds an [AnsiStyle].
+  String withStyle(final AnsiStyle style) {
+    return AnsiX.formatter.withStyle(this, style);
+  }
+
+  /// Adds [AnsiTextStyle] and colors.
+  String styled(
+    final AnsiTextStyle textStyle, [
+    final AnsiColor foreground = AnsiColor.none,
+    final AnsiColor background = AnsiColor.none,
+  ]) {
+    String text = this;
+    for (final AnsiStyle style in textStyle.styles) {
+      text = text.withStyle(style);
+    }
+    return text.colored(
+      foreground: foreground,
+      background: background,
+    );
+  }
+
+  /// Adds foreground color.
+  String withForegroundColor(final AnsiColor color) {
+    return AnsiX.formatter.withColor(this, foreground: color);
+  }
+
+  /// Adds background color.
+  String withBackgroundColor(final AnsiColor color) {
+    return AnsiX.formatter.withColor(this, background: color);
+  }
+
+  /// Adds colors.
+  String colored({
+    final AnsiColor foreground = AnsiColor.none,
+    final AnsiColor background = AnsiColor.none,
+  }) {
+    return AnsiX.formatter.withColor(
+      this,
+      foreground: foreground,
+      background: background,
+    );
+  }
+
+  /// Adds [Rgb] colors.
+  String coloredRgb({
+    final Rgb? foreground,
+    final Rgb? background,
+  }) {
+    return AnsiX.formatter.withColorRgb(
+      this,
+      foreground: foreground == null ? AnsiColor.none : AnsiColor.fromRgb(foreground),
+      background: background == null ? AnsiColor.none : AnsiColor.fromRgb(background),
+    );
+  }
+}
--- lib/src/core/extensions/string_extensions.dart
+++ lib/src/core/extensions/string_extensions.dart
@@ -1,26 +1,31 @@
 import 'package:ansix/ansix.dart';
 
 extension StringX on String {
+  /// Returns the string with no new line escape codes.
+  String get removeNewLines => replaceAll(AnsiEscapeCodes.newLine, '');
+
+  /// Returns the length of the string without counting any new line escape codes.
+  int get lengthWithoutNewLines => removeNewLines.length;
+
+  /// Returns the string with no whitespaces.
+  String get removeWhitespaces => replaceAll(RegExp(r'\s'), '');
+
   /// Wraps a text and returns the lines.
   List<String> wrapText({
     final int? fixedWidth,
     required final WrapOptions wrapOptions,
   }) {
     int? lineLength;
-
     if (fixedWidth != null && length > fixedWidth) {
       lineLength = fixedWidth;
     }
-
     if (wrapOptions.lineLength != null && length > wrapOptions.lineLength!) {
       lineLength = wrapOptions.lineLength!;
     }
 
     if (lineLength != null) {
-      final List<String> lines = split(AnsiEscapeCodes.newLine);
-
       return <String>[
-        for (final String line in lines)
+        for (final String line in split(AnsiEscapeCodes.newLine))
           ...line.splitEvery(
             lineLength,
             splitWords: wrapOptions.splitWords,
@@ -44,52 +49,16 @@ extension StringX on String {
       return matches.map((Match match) => match.group(0)?.trimLeft() ?? '').toList();
     }
 
-    final List<String> result = <String>[];
-
     if (lineBreak) {
-      int index = 0;
-      int? startIndex;
-      int? endIndex;
-
-      bool finished = false;
-      while (!finished) {
-        startIndex ??= index * length;
-        endIndex ??= (index + 1) * length;
-        if (endIndex >= this.length - 1) {
-          endIndex = this.length - 1;
-        }
-
-        final String segment = substring(startIndex, endIndex);
-        if (segment.trimRight().length >= length) {
-          endIndex--;
-          final String line = substring(startIndex, endIndex);
-          result.add(line.endsWith(' ') ? line.padRight(length) : '$line-');
-
-          if (endIndex >= this.length - 1) {
-            finished = true;
-            break;
-          }
-
-          startIndex = endIndex;
-          endIndex += length;
-        } else {
-          result.add(segment.trimLeft().padRight(length));
-
-          if (endIndex >= this.length - 1) {
-            finished = true;
-            break;
-          }
-
-          startIndex = null;
-          endIndex = null;
-        }
-
-        index++;
-      }
-
-      return result;
+      return splitWithLineBreaks(length);
     }
 
+    return splitWithoutLineBreaks(length);
+  }
+
+  /// Used to wrap a text with splitting words but no line breaks.
+  List<String> splitWithoutLineBreaks(final int length) {
+    final List<String> result = <String>[];
     final int count = (this.length / length).ceil();
 
     for (int i = 0; i < count; i++) {
@@ -102,117 +71,52 @@ extension StringX on String {
 
     return result;
   }
-}
 
-extension AnsiString on String {
-  /// Returns the string with no ANSI styles and colors.
-  String get unformatted => replaceAll(AnsiText.ansiMatcher, '');
+  /// Used to wrap a text with splitting words and line breaks.
+  /// Will add a '-' when a word is split.
+  List<String> splitWithLineBreaks(final int length) {
+    final List<String> result = <String>[];
 
-  /// Returns the length of the string with no ANSI styles and colors.
-  int get unformattedLength => unformatted.length;
+    int index = 0;
+    int? startIndex;
+    int? endIndex;
 
-  /// Returns the string with no new line escape codes.
-  String get removeNewLines => replaceAll(AnsiEscapeCodes.newLine, '');
+    bool finished = false;
+    while (!finished) {
+      startIndex ??= index * length;
+      endIndex ??= (index + 1) * length;
+      if (endIndex >= this.length - 1) {
+        endIndex = this.length - 1;
+      }
 
-  /// Returns the length of the string without counting any new line escape codes.
-  int get lengthWithoutNewLines => removeNewLines.length;
+      final String segment = substring(startIndex, endIndex);
+      if (segment.trimRight().length >= length) {
+        endIndex--;
+        final String line = substring(startIndex, endIndex);
+        result.add(line.endsWith(' ') ? line.padRight(length) : '$line-');
 
-  /// Returns the string with no whitespaces.
-  String get removeWhitespaces => replaceAll(RegExp(r'\s'), '');
+        if (endIndex >= this.length - 1) {
+          finished = true;
+          break;
+        }
 
-  /// Adds an [AnsiStyle].
-  String withStyle(final AnsiStyle style) {
-    return AnsiX.formatter.withStyle(this, style);
-  }
+        startIndex = endIndex;
+        endIndex += length;
+      } else {
+        result.add(segment.trimLeft().padRight(length));
 
-  /// Adds [AnsiTextStyle] and colors.
-  String styled(
-    final AnsiTextStyle textStyle, [
-    final AnsiColor foreground = AnsiColor.none,
-    final AnsiColor background = AnsiColor.none,
-  ]) {
-    String text = this;
-    for (final AnsiStyle style in textStyle.styles) {
-      text = text.withStyle(style);
+        if (endIndex >= this.length - 1) {
+          finished = true;
+          break;
+        }
+
+        startIndex = null;
+        endIndex = null;
+      }
+
+      index++;
     }
-    return text.colored(
-      foreground: foreground,
-      background: background,
-    );
+
+    return result;
   }
-
-  /// Adds foreground color.
-  String withForegroundColor(final AnsiColor color) {
-    return AnsiX.formatter.withColor(this, foreground: color);
-  }
-
-  /// Adds background color.
-  String withBackgroundColor(final AnsiColor color) {
-    return AnsiX.formatter.withColor(this, background: color);
-  }
-
-  /// Adds colors.
-  String colored({
-    final AnsiColor foreground = AnsiColor.none,
-    final AnsiColor background = AnsiColor.none,
-  }) {
-    return AnsiX.formatter.withColor(
-      this,
-      foreground: foreground,
-      background: background,
-    );
-  }
-
-  /// Adds [Rgb] colors.
-  String coloredRgb({
-    final Rgb? foreground,
-    final Rgb? background,
-  }) {
-    return AnsiX.formatter.withColorRgb(
-      this,
-      foreground: foreground == null ? AnsiColor.none : AnsiColor.fromRgb(foreground),
-      background: background == null ? AnsiColor.none : AnsiColor.fromRgb(background),
-    );
-  }
-}
-
-extension AnsiXShortcuts on String {
-  /// Adds [AnsiStyle.bold] text style on a string.
-  String bold() => withStyle(AnsiStyle.bold);
-
-  /// Adds [AnsiStyle.dim] text style on a string.
-  String dim() => withStyle(AnsiStyle.dim);
-
-  /// Adds [AnsiStyle.italic] text style on a string.
-  String italic() => withStyle(AnsiStyle.italic);
-
-  /// Adds [AnsiStyle.underline] text style on a string.
-  String underline() => withStyle(AnsiStyle.underline);
-
-  /// Adds [AnsiStyle.strikethrough] text style on a string.
-  String strikethrough() => withStyle(AnsiStyle.strikethrough);
-
-  /// Adds [AnsiColor.black] foreground color on a string.
-  String black() => colored(foreground: AnsiColor.black);
-
-  /// Adds [AnsiColor.red] foreground color on a string.
-  String red() => colored(foreground: AnsiColor.red);
-
-  /// Adds [AnsiColor.green] foreground color on a string.
-  String green() => colored(foreground: AnsiColor.green);
-
-  /// Adds [AnsiColor.yellow] foreground color on a string.
-  String yellow() => colored(foreground: AnsiColor.yellow);
-
-  /// Adds [AnsiColor.blue] foreground color on a string.
-  String blue() => colored(foreground: AnsiColor.blue);
-
-  /// Adds [AnsiColor.fuchsia] foreground color on a string.
-  String fuchsia() => colored(foreground: AnsiColor.fuchsia);
-
-  /// Adds [AnsiColor.aqua] foreground color on a string.
-  String aqua() => colored(foreground: AnsiColor.aqua);
-
-  /// Adds [AnsiColor.white] foreground color on a string.
-  String white() => colored(foreground: AnsiColor.white);
 }

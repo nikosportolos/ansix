0514bb44edecbbef84e342780175baed15fc4483
Nikos Portolos <nikos.portolos@gmail.com>
2024-09-30 04:01:54 +0300

    remove unnecessary library name

--- lib/ansix.dart
+++ lib/ansix.dart
@@ -1,7 +1,3 @@
-/// AnsiX is a powerful and easy-to-use library that provides tools and extensions
-/// for adding ANSI color and styling support to your Dart & Flutter applications.
-library ansix;
-
 export 'src/ansix.dart' hide initializeAnsiX;
 export 'src/core/core.dart';
 export 'src/formatter/formatter.dart' show ColorFormat;
